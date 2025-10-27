5aed612255cc0011be0801ed793f1918bed4a979
shukawam <shukawam+github@gmail.com>
2025-10-27 23:06:16 +0900

    Initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+import pytest
+
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+def test_greet(client):
+    response = client.get("/greet")
+    assert response.status_code == 200
+    assert response.data == b"Hello world"
