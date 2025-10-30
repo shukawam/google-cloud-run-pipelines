ea546fa3f540c79599e9eec12ca5cfb4cf513727
shukawam <shukawam+github@gmail.com>
2025-10-30 11:51:50 +0900

    Update return message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/greet")
 def greet():
-    return "Hello world"
+    return "Hello world - demo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
